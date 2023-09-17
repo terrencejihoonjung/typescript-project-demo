570a078a7a6b5a984c686fe72c1e8513ad8826fc
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-16 19:50:59 -0700

    getters and setters

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,24 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Jaipur";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    // cannot make setters a void return type
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNumber;
+    }
+    deleteToken() {
+        console.log("Token deleted");
     }
 }
 const terrence = new User("terrence@gmail.com", "terrence");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 class User {
+  private _courseCount: number = 1;
+
   readonly city: string = "Jaipur";
   constructor(public email: string, public name: string) {}
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  // cannot make setters a void return type
+  set courseCount(courseNumber: number) {
+    if (courseNumber <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNumber;
+  }
+
+  private deleteToken(): void {
+    console.log("Token deleted");
+  }
 }
 
 const terrence = new User("terrence@gmail.com", "terrence");
